7b6f159a4d788bfc5bd8d27d515bf29e6038b478
Andrew Dinh <andrewd@benchling.com>
2020-12-29 18:06:55 -0800

    Able to filter the list of pokemon

--- src/App.js
+++ src/App.js
@@ -30,11 +30,14 @@ const FilterInput = () => {
 
 const PokemonTable = () => {
   const pokemon = useStore((state) => state.pokemon);
+  const filter = useStore((state) => state.filter);
 
   return (
     <table width="100%">
       <tbody>
-        {pokemon.map(({ id, name: { english }, type }) => (
+        {pokemon
+        .filter(({ name: {english} }) => english.toLowerCase().includes(filter.toLowerCase()))
+        .map(({ id, name: { english }, type }) => (
           <tr key={id}>
             <td>{english}</td>
             <td>{type.join(', ')}</td>
